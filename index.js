c3e8b3eb5750d8b19cf1865d998ee4b35b3df616
Adrian Alberto <adrianalberto1986@gmail.com>
2022-04-24 15:38:03 -0500

    commit tree 26caf5000f050be6aa6d90eec3c47ff398833dcc

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity){
+    if(activity == null){
+        return "This Saturday, I want to roller-skate!"
+    } else {
+        return `This Saturday, I want to ${activity}!`
+    }
+}
+
+function mondayWork(activity){
+    if(activity == null){
+        return "This Monday, I will go to the office."
+    } else {
+        return `This Monday, I will ${activity}.`
+    }
+}
+
+function wrapAdjective(special){
+    return function encouragingPromptFunction(value){
+        return `You are ${special}${value}${special}!`
+    }
+}
+
